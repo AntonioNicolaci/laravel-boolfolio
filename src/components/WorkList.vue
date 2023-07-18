40086c40467f4d702c06a227e236a8acd66c8a75
antonionicolaci <antonionicolaci48@gmail.com>
2023-07-18 20:07:47 +0200

    add file / add api get

---
+++ src/components/WorkList.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            arrWork: [],
+        };
+    },
+    methods: {
+        getAPIWork() {
+            axios.get('http://localhost:8000/api/works')
+                .then(response => console.log(response))
+        }
+    },
+    created() {
+        this.getAPIWork();
+    },
+}
+</script>
+
+<template>
+  <div>
+    ciaio ma 2
+  </div>
+  
+</template>
+
+<style lang="scss">
+  
+</style>
