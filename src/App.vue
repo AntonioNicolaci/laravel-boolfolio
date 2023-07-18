52af472ff2572dd0354f5965e58c1bc699693ef5
antonionicolaci <antonionicolaci48@gmail.com>
2023-07-18 20:07:36 +0200

    add file

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import WorkList from './components/WorkList.vue';
+
+  export default {
+    components: {
+      WorkList,
+    },
+  }
+</script>
+
+<template>
+  <div>
+    ciaio 
+  </div>
+  <WorkList></WorkList>
+</template>
+
+<style lang="scss">
+  
+</style>
